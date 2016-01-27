fbb46039bd35c4f9f09bec60a75646b8957829a0
Clay Sissing <claysissing@gmail.com>
2016-01-27 02:22:31 +0000

    fingers crossed

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@
 
     //This should be use for prod build as it bundles css/js
     gulp.task('usemin', function () {
-        return gulp.src('~/clone/build/test/**/*.html')
+        return gulp.src('./build/test/**/*.html')
             .pipe(usemin({
                 css: [minifyCss, rev],
                 html: [ function () {return minifyHtml({ empty: true });} ],
