8ca2fb216b7237cd41b5013c99d9b091744d395a
Clay Sissing <claysissing@gmail.com>
2016-06-23 21:56:38 +0100

    changes to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@
 
     //This should be use for prod build as it bundles css/js
     gulp.task('usemin', function () {
-        return gulp.src('./build/**/*.html')
+        return gulp.src('~/clone/build/**/*.html')
             .pipe(usemin({
                 css: [minifyCss, rev],
                 html: [ function () {return minifyHtml({ empty: true });} ],
