7df7918e41e7c2207a9bdf49babe676e912270d3
claysissing <claysissing@gmail.com>
2017-04-23 18:52:07 +0100

    moved tp sparkpost from mandrill - 1.11.0

--- src/php/debt.form.php
+++ src/php/debt.form.php
@@ -11,22 +11,31 @@
 
     $captchaResult = $google->checker();
     $captchaResultDecode = json_decode($captchaResult, true);
-    $emailResult = $sparkPost->createEmailSend($_POST, json_encode(["FULLNAME" => $_POST['fullName']]));
+    $emailResult = $sparkPost->createEmailSend($_POST, json_encode(["FULLNAME" => $_POST['fullName']], 'apply-c'));
     $adminEmail = $sparkPost->createEmailSend(
         $_POST,
         json_encode([
-            "FULLNAME" => $_POST['fullName'], 
-            "EMAIL" => $_POST['email'], 
-            "CONTACT" => $_POST['number'], 
-            "MESSAGE" => $_POST['enquiry']
+            "FULLNAME" => $_POST['fullName'],
+            "NUMBERS" => $_POST['number'],
+            "EMAILS" => $_POST['email'],
+            "CREDITORS" => $_POST['creditors'],
+            "DEBTS" => $_POST['debt'],
+            "REVIEWS" => $_POST['review'],
+            "BLACKLISTEDS" => $_POST['blacklisted'],
+            "GARNISHEES" => $_POST['garnishees'],
+            "ARREARS" => $_POST['arrears'],
+            "POSITIONS" => $_POST['position'],
+            "LOCATIONS" => $_POST['location'],
+            "EMPLOYEDS" => $_POST['employed'],
+            "MARRIEDS" => $_POST['married']
         ]),
-        'touch-internal',
+        'apply',
         [
             "email" => "info@libertineconsultants.co.za",
             "name" => $_POST['fullName']
         ]
     );
-    $agile->sendAgileData($_POST, 'Get in touch');
+    $agile->sendAgileData($_POST, 'Apply for debt relief');
 
     if ($captchaResultDecode->success === true) {
         $data = json_decode($emailResult, true);
