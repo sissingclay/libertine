23f15b97184706d1e73686f72c62155373d6e013
claysissing <claysissing@gmail.com>
2017-04-23 18:10:11 +0100

    moved templates to sparkpost from mandrill - 1.9.0

---
+++ src/php/agile.class.php
@@ -0,0 +1,50 @@
+<?php
+    include 'curlwrap_v2.php';
+    class Agile {
+        public function sendAgileData ($data, $tag) {
+            $contact_email = $data['email'];
+            $contact_json = array(
+                "tags"=>array($tag),
+                "properties"=>array(
+                    array(
+                        "name" => "first_name",
+                        "value" => $data['fullName'],
+                        "type" => "SYSTEM"
+                    ),
+                    array(
+                        "name" => "email",
+                        "value" => $contact_email,
+                        "type" => "SYSTEM"
+                    ),  
+                    array(
+                        "name" => "phone",
+                        "value" => $data['number'],
+                        "type" => "SYSTEM"
+                    ),
+                )
+            );
+
+            $contact_json = json_encode($contact_json);
+            $result = curl_wrap("contacts", $contact_json, "POST", "application/json");
+            $result = json_decode($result, false, 512, JSON_BIGINT_AS_STRING);
+            $contact_id = $result->id;
+
+            if ($data['enquiry']) {
+                $descr = $data['enquiry'];
+            }
+
+            if ($data['creditors']) {
+                $descr = 'creditors: ' .$data['creditors']. ', debt: ' .$data['debt']. ', review: ' .$data['review']. ', blacklisted: ' .$data['blacklisted']. ', garnishees: ' .$data['garnishees']. ', arrears: ' .$data['arrears']. ', position: ' .$data['position']. ', location: ' .$data['location']. ', employeds: ' .$data['employeds']. ', married: ' .$data['married'];
+            }
+
+            $note_json = array(
+                "subject" => "Website enquiry",
+                "description" => $descr,
+                "contact_ids" => array($contact_id)
+            );
+
+            $note_json = json_encode($note_json);
+            curl_wrap("notes", $note_json, "POST", "application/json");
+        }
+    }
+?>
---
+++ src/php/touch.form.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('sparkpost.class.php');
+    require_once('google.validator.php');
+    require_once('agile.class.php');
+
+    header('Content-Type: application/json;charset=utf-8');
+
+    $google = new GoogleValidator($_POST['g-recaptcha-response']);
+    $sparkPost = new SparkPost();
+    $agile = new Agile();
+
+    $captchaResult = $google->checker();
+    $captchaResultDecode = json_decode($captchaResult, true);
+    $emailResult = $sparkPost->createEmailSend($_POST, json_encode(["FULLNAME" => $_POST['fullName']]));
+    $adminEmail = $sparkPost->createEmailSend(
+        $_POST,
+        json_encode([
+            "FULLNAME" => $_POST['fullName'], 
+            "EMAIL" => $_POST['email'], 
+            "CONTACT" => $_POST['number'], 
+            "MESSAGE" => $_POST['enquiry']
+        ]),
+        'touch-internal',
+        [
+            "email" => "claysissing@gmail.com",
+            "name" => $_POST['fullName']
+        ]
+    );
+    $agile->sendAgileData($_POST, 'Get in touch');
+
+    if ($captchaResultDecode->success === true) {
+        $data = json_decode($emailResult, true);
+        echo json_encode($data);
+    } else {
+        http_response_code(500);
+    }
+?>
