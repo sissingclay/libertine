427700d96ff9ea18eba9f3bf2ad8dacb878d788b
Clay Sissing <clay@mylocalpitch.com>
2018-09-08 18:09:19 +0100

    show user date and links, 1.19.0

--- src/node/contentful.js
+++ src/node/contentful.js
@@ -32,46 +32,57 @@ client.getEntries({
     blogEnties(contentType.items)
   })
 
-var blogEnties = (blogsArticles) => {
-  fs.writeFile(
-    './build/blog.html',
-    pug.renderFile(
-      './src/node/blogList.pug',
-      {
-        pretty: true,
-        lists: blogsArticles
-      }
-    ),
-    function (err) {
-      if (err) return console.log(err)
-      console.log('errrr')
-    }
-  )
-  blogsArticles.forEach((article) => {
+var blogEnties = (blogArticles) => {
+    const updatedBlogArticles = [];
+
+    blogArticles.forEach((article) => {
+        article.fields.date = (article.fields.date) ? dateFormat(article.fields.date, 'dd/mm/yyyy') : '';
+        article.fields.body = markdown.toHTML(article.fields.body);
+        article.fields.bodySection1 = markdown.toHTML(article.fields.bodySection1);
+        article.fields.bodySection2 = (article.fields.bodySection3) ? markdown.toHTML(article.fields.bodySection2) : '';
+        article.fields.bodySection3 = (article.fields.bodySection3) ? markdown.toHTML(article.fields.bodySection3): '';
+        article.fields.bodySection4 = (article.fields.bodySection4) ? markdown.toHTML(article.fields.bodySection4) : '';
+
+        updatedBlogArticles.push(article);
+
+        fs.writeFile(
+            dir + '/' + article.fields.slug + '.html',
+            pug.renderFile('./src/node/blog.pug',
+            {
+                pretty: true,
+                blog: {
+                    title: article.fields.title,
+                    date: article.fields.date,
+                    time: article.fields.date,
+                    body: article.fields.body,
+                    bodySection1: article.fields.bodySection1,
+                    bodySection2: article.fields.bodySection2,
+                    bodySection3: article.fields.bodySection3,
+                    bodySection4: article.fields.bodySection4,
+                    metaTitle: article.fields.metaTitle,
+                    metaDescription: article.fields.metaDescription
+                }
+            }),
+            function (err) {
+                if (err) return console.error(err)
+                console.log(article.fields.slug + '.html')
+            }
+        )
+    });
+
     fs.writeFile(
-      dir + '/' + article.fields.slug + '.html',
-      pug.renderFile('./src/node/blog.pug',
-        {
-          pretty: true,
-          blog: {
-            title: article.fields.title,
-            date: dateFormat(article.fields.date, 'dd/mm/yyyy'),
-            time: dateFormat(article.fields.date, 'yyyy/mm/dd'),
-            body: markdown.toHTML(article.fields.body),
-            bodySection1: markdown.toHTML(article.fields.bodySection1),
-            bodySection2: markdown.toHTML(article.fields.bodySection2),
-            bodySection3: (article.fields.bodySection3) ? markdown.toHTML(article.fields.bodySection3) : '',
-            bodySection4: (article.fields.bodySection4) ? markdown.toHTML(article.fields.bodySection4) : '',
-            metaTitle: article.fields.metaTitle,
-            metaDescription: article.fields.metaDescription
-          }
+        './build/blog.html',
+        pug.renderFile(
+            './src/node/blogList.pug',
+            {
+                pretty: true,
+                lists: updatedBlogArticles
+            }
+        ),
+        function (err) {
+            if (err) return console.error(err)
+            console.error('errrr')
         }
-      ),
-      function (err) {
-        if (err) return console.log(err)
-        console.log(article.fields.slug + '.html')
-      }
     )
-  })
 }
 
