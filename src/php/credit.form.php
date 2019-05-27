2c595e77bff1b4d8f0d8c059a536b58e5a06d4c0
Clay Sissing <clay@mylocalpitch.com>
2019-05-27 16:53:57 +0100

    Updated the credit analysis form, 2.1.0

--- src/php/credit.form.php
+++ src/php/credit.form.php
@@ -8,37 +8,44 @@
     $google = new GoogleValidator($_POST['g-recaptcha-response']);
     $sparkPost = new SparkPost();
     $agile = new Agile();
+    $agileCredit = new AgileCreditAnalysis();
 
     $captchaResult = $google->checker();
     $captchaResultDecode = json_decode($captchaResult, true);
     $fullName = $_POST['name'] . ' ' . $_POST['surname'];
-    $emailResult = $sparkPost->createEmailSend(
-        $_POST, 
-        json_encode(["FULLNAME" => $fullName]), 
-        'apply-c',
-        [
-            "email" => $_POST['email'],
-            "name" => $fullName
-        ]
-    );
-    $adminEmail = $sparkPost->createEmailSend(
-        $_POST,
-        json_encode([
-            "NAME" => $_POST['name'],
-            "SURNAME" => $_POST['surname'],
-            "EMAIL" => $_POST['email'], 
-            "CONTACT" => $_POST['number'], 
-            "IDNUMBER" => $_POST['idNumber']
-        ]),
-        'credit_analysis_internal',
-        [
-            "email" => "info@libertineconsultants.co.za",
-            "name" => 'Credit analysis'
-        ]
-    );
-    $agile->sendAgileData($_POST, 'Credit analysis');
 
     if ($captchaResultDecode->success === true) {
+        $emailResult = $sparkPost->createEmailSend(
+            $_POST, 
+            json_encode(["FULLNAME" => $fullName]), 
+            'apply-c',
+            [
+                "email" => $_POST['email'],
+                "name" => $fullName
+            ]
+        );
+        
+        $adminEmail = $sparkPost->createEmailSend(
+            $_POST,
+            json_encode([
+                "NAME" => $_POST['name'],
+                "SURNAME" => $_POST['surname'],
+                "EMAIL" => $_POST['email'], 
+                "CONTACT" => $_POST['number'], 
+                "IDNUMBER" => $_POST['idNumber'],
+                "PREFERREDCONTACTMETHOD" => $_POST['contactMethod'],
+                "WHENTOCALL" => $_POST['callAt'],
+            ]),
+            'credit_analysis_internal',
+            [
+                "email" => "clay@libertineconsultants.co.za",
+                "name" => 'Credit analysis'
+            ]
+        );
+
+        $contact_id = $agile->sendAgileData($_POST, 'Credit analysis');
+        $agileCredit->sendData($contact_id, $_POST);
+
         $data = json_decode($emailResult, true);
         echo json_encode($data);
     } else {
